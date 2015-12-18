825feeebfc135aa83b483b5f4f0b5d9e9aa05f37
Hendrik Holtmann <holtmann@mac.com>
2015-12-18 15:30:19 +0100

    Fixed compile on OS X 10.11/ Xcode 7.x

--- Classes/smcWrapper.h
+++ Classes/smcWrapper.h
@@ -21,7 +21,6 @@
  *	Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
  */
 
-#include <openssl/evp.h>
 #import <Cocoa/Cocoa.h>
 #import <smc.h>
 #import <MachineDefaults.h>
